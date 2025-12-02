dfa61544419ea77a33ce48f4ba2c8f98c92c04a5
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2025-12-01 19:34:23 -0800

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { languages } from "./languages";
 
-export default function App() {
-  return <main>Game here</main>;
+export default function AssemblyEndgame() {
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the world within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win</h2>
+        <p>Well done! 🔥</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+    </main>
+  );
 }
